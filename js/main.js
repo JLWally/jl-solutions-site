bad885b30b0880aaf377bdbb0cafdbcfee4b23bb
JessWalcott <103128119+JessWalcott@users.noreply.github.com>
2025-08-04 14:56:25 -0400

    Site refresh: added Eligibility Wizard page, Fix My App updates, branding improvements

--- js/main.js
+++ js/main.js
@@ -1,13 +1,1 @@
-// Injects header and footer into each page
-
-fetch("/partials/header.html")
-  .then(res => res.text())
-  .then(data => {
-    document.getElementById("header").innerHTML = data;
-  });
-
-fetch("/partials/footer.html")
-  .then(res => res.text())
-  .then(data => {
-    document.getElementById("footer").innerHTML = data;
-  });
+// Custom scripts
