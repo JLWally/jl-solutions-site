00db95ff65a7e04add0b351c1f6cb25bbb699332
Jess Wally <jesswally@capture411.com>
2026-01-30 15:09:57 -0500

    Remove basePath - will use proxy instead

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: '/apps/trailcrafter',
-  assetPrefix: '/apps/trailcrafter',
   images: {
     domains: ['localhost', 'jlsolutions.io', 'trailcrafter.netlify.app'],
   },
